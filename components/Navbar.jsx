3d40add1dd07225d9ec500a4405bb9d953df1a39
Marios Kanellopoulos <mariosknl@outlook.com>
2023-05-10 18:40:39 +0300

    finalizing project

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
@@ -24,7 +23,7 @@ const Navbar = () => {
 	return (
 		<nav className="flex-between w-full mb-16 pt-3">
 			<Link href="/" className="flex gap-2 flex-center">
-				<Image
+				<img
 					src="/assets/images/logo.svg"
 					alt="Promptopia logo"
 					width={30}
